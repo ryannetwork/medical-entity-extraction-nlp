7c5c78396325bd58ddb70c9d9b6445e405e04ae4
Akshay Shah <akshay.vjti11@gmail.com>
2019-10-21 13:42:59 -0700

    cleaned up the preprocessing code

---
+++ code/data_preprocessing/constants.py
@@ -0,0 +1,3 @@
+CLEANED_FILES_PATH = "../../medical_data/train_data/cleaned_files"
+PARTNER_CONCEPT_FILES_PATH = "../../medical_data/train_data/training_data/partners/concept"
+PARTNER_REPORT_FILES_PATH = "../../medical_data/train_data/training_data/partners/txt"
